e13d7090a4a16dc5eaaa052a5a3645144f2e5e59
timog448@gmail.com <user.email: timog448@gmail.com>
2026-01-18 17:14:18 +0100

    new beginning

--- maintenance/backups/restore.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Script to restore backups
-# TODO: Implement restore logic
-
-echo "Restore script not yet implemented."
--- maintenance/updates/update_system.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Script for system updates (apt update/upgrade)
-# Can be executed on the Proxmox host or in containers/VMs.
-
-echo "Starting system updates..."
-apt-get update && apt-get upgrade -y
-echo "System updates completed."
